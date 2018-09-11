0d829d0d64e571fbbfb71acc0689110bc0269f86
Taiga Hirano <taiga.hirano@gmail.com>
2018-09-12 02:34:46 +0900

    Finish singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   resources :members do
     get "search", on: :collection
   end
+  resource :session, only: [:create, :destroy]
+  resource :account, only: [:show, :edit, :update]
+  resource :password, only: [:show, :edit, :update]
 end
